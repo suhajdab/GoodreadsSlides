633dd0732aeda5479d6f063f3813f01f3ece20f0
Balázs Suhajda <suhajdab@gmail.com>
2024-11-14 14:38:52 +0100

    Initial Commit

---
+++ GoodreadsSlides/GoodreadsSlidesView.h
@@ -0,0 +1,12 @@
+//
+//  GoodreadsSlidesView.h
+//  GoodreadsSlides
+//
+//  Created by Balázs Suhajda on 2024-11-14.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface GoodreadsSlidesView : ScreenSaverView
+
+@end
---
+++ GoodreadsSlides/GoodreadsSlidesView.m
@@ -0,0 +1,51 @@
+//
+//  GoodreadsSlidesView.m
+//  GoodreadsSlides
+//
+//  Created by Balázs Suhajda on 2024-11-14.
+//
+
+#import "GoodreadsSlidesView.h"
+
+@implementation GoodreadsSlidesView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
